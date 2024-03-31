f4192fd66163bbeaa53ffc276e9cfb2eef07caa7
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-31 10:25:29 -0300

    bigint

--- build/module-02/number.js
+++ build/module-02/number.js
@@ -1,1 +1,19 @@
 "use strict";
+// => Exemples: number
+let num1 = 32.0;
+let num2 = 0x78cf;
+let num3 = 0o577;
+let num4 = 0b110001;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+// => Example: bigint
+let big1 = 90019273123729n;
+let big2 = 90243946122n;
+let big3 = 0x1238917ab87d9a8b7f98n;
+let big4 = 140737488355331n;
+console.log(big1);
+console.log(big2);
+console.log(big3);
+console.log(big4);
---
+++ src/module-02/number.ts
@@ -0,0 +1,23 @@
+// => Exemples: number
+
+let num1: number = 32.0;
+let num2: number = 0x78cf;
+let num3: number = 0o577;
+let num4: number = 0b110001;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+// => Example: bigint
+
+let big1: bigint = 90019273123729n;
+let big2: bigint = 0b1010100000010111101010101011010001010n;
+let big3: bigint = 0x1238917ab87d9a8b7f98n;
+let big4: bigint = 0o4000000000000003n;
+
+console.log(big1);
+console.log(big2);
+console.log(big3);
+console.log(big4);
