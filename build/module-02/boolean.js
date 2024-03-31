b29ac5d71f279b0aa650eadc31632aac522a41b5
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-31 10:10:34 -0300

    boolean

--- build/module-02/boolean.js
+++ build/module-02/boolean.js
@@ -1,2 +1,14 @@
 "use strict";
-let variable = true;
+// => Example 01
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+// => Example 02
+let concluido = false;
+if (concluido) {
+    console.log('Tarefa concluida com sucesso.');
+}
+else {
+    console.log('Tarefa pendente!');
+}
--- src/module-02/boolean.ts
+++ src/module-02/boolean.ts
@@ -1,2 +1,19 @@
-let variable: boolean = true;
 
+
+// => Example 01
+
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+// => Example 02
+
+let concluido: boolean = false;
+
+if(concluido){
+    console.log('Tarefa concluida com sucesso.');
+} else {
+    console.log('Tarefa pendente!');
+}
