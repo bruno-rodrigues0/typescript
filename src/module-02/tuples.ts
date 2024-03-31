ad11e1c718d266bc0e978b9d31dee08b57807ced
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-31 13:58:05 -0300

    tuples

--- src/module-02/tuples.ts
+++ src/module-02/tuples.ts
@@ -32,3 +32,30 @@ frutas1= ['abaxaki', 'lajanra', 'çamâ', 'meçanlia', 'ganma'];
 console.log(frutas1)
 console.log(...frutas1)
 
+// ==> Exemplo 05
+
+let listaFrutas: [number, boolean, ...string[]] = [5, true, ...frutas1]
+
+console.log(listaFrutas);
+
+// ==> Exmeplo 06
+
+function lsitarPessoals(infos:[nome:string, idade:number]) {
+        return [...infos];
+}
+
+let resultado = lsitarPessoals(['Bruno', 15])
+console.log(resultado)
+
+
+// ==> Exemplo 07
+
+type Nome = 
+    | [primeiroNome: string, sobrenome: string]
+    | [primeiroNome: string, nomeMeio: string, sobrenome: string]
+
+function criarPessoa(...nome: Nome){
+    return [...nome];
+}
+
+console.log(criarPessoa('Bruno', 'Rodrigues', 'e Silva'))
