b73104df88f503fd8aa5543cedf29966073e2c37
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-31 13:03:09 -0300

    strings

---
+++ src/module-02/strings.ts
@@ -0,0 +1,26 @@
+let string :string = 'string';
+let nome: string = 'Bruno';
+
+string = string.split('').reverse().join('')
+
+// split atring to an array
+// string = ['s', 't', 'r', 'i', 'n', 'g']
+// reverse array string
+// join array to a string
+// 'gnirts' 
+
+console.log(string)
+console.log(nome)
+
+
+let funcionario = {
+    user: 'Bruno',
+    funcao: 'Desenvolvedor'
+};
+
+console.log(funcionario)
+console.log(typeof(funcionario))
+
+// funcionario.user = 2
+
+funcionario.user = 'Gabriel'
