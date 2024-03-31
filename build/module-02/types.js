34ec9ad5514716b1c4f61c9bb27bf7efad053a4a
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-31 10:12:20 -0300

    aa

---
+++ build/module-02/types.js
@@ -0,0 +1,16 @@
+"use strict";
+// => Variables [Type Annotation]
+let nome = 'Bruno Rodrigues';
+console.log(nome);
+// => Arrays [Type Annotation]
+let animais = ['Elefante', 'Gato', 'Cachorro', 'Panda'];
+console.log(animais);
+// => Objects [Type Annotation]
+let carro;
+carro = { nome: 'Toyota Supra', ano: 2020, preco: 500000.00 };
+console.log(carro);
+// => Functions [Type Annotation]
+function multiplicarNumeros(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicarNumeros(2, 4));
