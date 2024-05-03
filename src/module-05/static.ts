f095b885f2281afa9da2e4bd99511a3b5a36d9cb
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-05-03 13:27:04 -0300

    static

---
+++ src/module-05/static.ts
@@ -0,0 +1,24 @@
+export {};
+
+// ==> Exemplo 01
+
+class Funcionario {
+    static contratacoes = 0;
+
+    private _nome: string;
+    private _sobrenome: string;
+    private _titulo: string;
+
+    constructor(nome: string, sobrenome: string, titulo: string) {
+        Funcionario.contratacoes++;
+
+        this._nome = nome;
+        this._sobrenome = sobrenome;
+        this._titulo = titulo;
+    }
+}
+
+const funcionario_01 = new Funcionario("Bruno", "Rodrigues", "Dev Sênior");
+const funcionario_02 = new Funcionario("Irineu", "Junio", "Dev Sênior");
+
+console.log(Funcionario.contratacoes);
