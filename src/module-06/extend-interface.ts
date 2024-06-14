2d3d4faa8bd6162a42b9d9f54e0aedc465857107
bruno-rodrigues0 <brunorodriguesmtv0@gmail.com>
2024-06-14 18:35:02 -0300

    omit

--- src/module-06/extend-interface.ts
+++ src/module-06/extend-interface.ts
@@ -18,3 +18,48 @@ const cachorro: Cachorro = {
 }
 
 console.log(cachorro)
+
+
+// ==> Exemplo 02 
+
+interface Cachorro_01 {
+    nome: string;
+}
+
+interface Gato {
+    idade: number;
+}
+
+interface Animal_01 extends Cachorro_01, Gato {
+    porte: number;
+}
+
+const animal: Animal_01 = {
+    nome: "Farora",
+    idade: 4,
+    porte: 0.6
+}
+
+console.log(animal)
+
+// ==> Exemplo 03
+
+interface Funcionario {
+    id: number;
+    nome: string;
+    salario: number;
+}
+
+interface Desenvolvedor extends Omit<Funcionario, 'id'> {
+    id: string;
+    linguagem: string;
+}
+
+const funcionario: Desenvolvedor = {
+    id: '1n23j-3123nb',
+    nome: 'Bruno',
+    salario: 1289,
+    linguagem: 'TypeScript'
+}
+
+console.log(funcionario)
