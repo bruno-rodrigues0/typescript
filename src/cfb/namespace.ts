0b87cb07f9b5b0e72c139758c2396dd9e9e929ef
bruno-rodrigues0 <brunorodriguesmtv0@gmail.com>
2024-06-18 15:16:58 -0300

    namespaces

--- src/cfb/namespace.ts
+++ src/cfb/namespace.ts
@@ -3,25 +3,38 @@ export {}
 namespace Veiculo {
     export class Carro {
         nome: string
+        motor: Motores.Motor
 
-        constructor(nome: string) {
+        constructor(nome: string, motor: Motores.Motor) {
             this.nome = nome
+            this.motor = motor
         }
     }
 }
 
 namespace Motores {
+    class Turbo {
+        pot: number
+
+        constructor(pot: number) {
+            this.pot = pot
+        }
+    }
+
+    const turbo = new Turbo(200)
+
     export class Motor {
         potencia: number
 
         constructor(pot: number) {
-            this.potencia = pot
+            this.potencia = pot + turbo.pot 
         }
     }
 
 }
 
-const supra = new Veiculo.Carro("Supra")
 const v8 = new Motores.Motor(400)
+const supra = new Veiculo.Carro("Supra", v8)
 
-console.log(supra, v8)
+
+console.log(supra)
