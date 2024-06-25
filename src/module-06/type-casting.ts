5e6f60824515c33e2e9c087f565ef877b67bb454
bruno-rodrigues0 <brunorodriguesmtv0@gmail.com>
2024-06-25 17:32:21 -0300

    casting

--- src/module-06/type-casting.ts
+++ src/module-06/type-casting.ts
@@ -5,3 +5,17 @@ const valor: unknown = "Ola mundo";
 console.log((valor as string).length);
 console.log((<string>valor).length);
 
+// Exemplo 2
+
+const carro = 'Fusca';
+
+const tamahoString: number = (<string>carro).length;
+
+
+console.log("O tamanho da string eh : ", tamahoString);
+
+// Exemplo 3
+
+const value = <HTMLElement>document.getElementById('input');
+
+console.log(value);
